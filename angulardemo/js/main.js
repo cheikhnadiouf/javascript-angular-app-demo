a5bcf40bbfe682b46ba6cf53117f333d97da19fa
Systemaker <info@systemaker.com>
2017-02-09 06:42:32 +0100

    Remove obsolete token handler service sample

--- angulardemo/js/main.js
+++ angulardemo/js/main.js
@@ -18,69 +18,6 @@ app.config(function($routeProvider, $locationProvider) {
 
 
 
-// USER TOKEN HANDLER
-//-------------------------------------------------------------------------------------
-/*
-app.factory('tokenHandler', function($rootScope, $http, $q) {
-  var token = null,
-      currentUser;
- 
-  var tokenHandler = {
-    // store our token for later retrieval
-    set: function(v) { token = v; },
-    get: function() {
-      if (!token) {
-        $rootScope.$broadcast('event:unauthorized');
-      }
-      else {
-        return token;
-      }
-    },
-    getCurrentUser: function() {
-      var d = $q.defer();
-
-      if (currentUser) {
-        d.resolve(currentUser);
-      } else {
-        $http({
-          url: '/api/current_user',
-          method: 'POST'
-        }).then(function(data) {
-          d.resolve(data.data);
-        });
-      }
-      return d.promise;
-    },
-    wrapActions: function(r, actions) {
-      // copy original resource
-      var wrappedResource = r;
-      for (var i=0; i < actions.length; i++) {
-        tokenWrapper( wrappedResource, actions[i] );
-      }
-      // return modified copy of resource
-      return wrappedResource;
-    }
-  };
-  
-  // https://gist.github.com/nblumoe/3052052
-  var tokenWrapper = function(resource, action) {
-    // copy original action
-    resource['_' + action]  = resource[action];
-    // create new action wrapping the original and sending token
-    resource[action] = function( data, success, error){
-      return resource['_' + action](
-        angular.extend({}, data || {}, {access_token: tokenHandler.get()}),
-        success,
-        error
-      );
-    };
-  };
- 
-  return tokenHandler;
-}); */
-
-
-
 // TRANSLATION DYNAMIC I18N ANGULAR FILES WITH tmhDynamicLocale
 //-------------------------------------------------------------------------------------
 app.config(function(tmhDynamicLocaleProvider) {
@@ -452,24 +389,6 @@ app.controller("IndexCtrl", function($scope, $location, Item) {
       }
         
       
-      /*
-      $http({
-        url: '/api/users',
-        method: 'POST',
-        data: { user: $scope.user }
-      }).success(function(data) {
-        // If the request is successful, and the user
-        // has been created: store the auth_token,
-        // broadcast the 'authenticated' event across
-        // our app, and redirect to '/'
-        tokenHandler.set( data.auth_token );
-        $scope.$broadcast('event:authenticated');
-        $location.path('/');
-        flash.setMessage("successively signed in", "success");
-      }).error(function(reason) {
-        // Store the errors to format on screen
-        $scope.user.errors = reason;
-      });*/
     };
   });
   
